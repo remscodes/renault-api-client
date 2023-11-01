2c5dbd7888e7f48b0e60b19225b0a4ef24c90bbb
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2023-11-01 20:16:50 +0100

    Add rollup and remove not used dependencies

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+import terser from '@rollup/plugin-terser';
+import bundleSize from 'rollup-plugin-bundle-size'
+import dts from 'rollup-plugin-dts';
+import esbuild from 'rollup-plugin-esbuild';
+
+/** @typedef {import('rollup').RollupOptions} RollupOptions */
+/** @typedef {import('rollup').OutputOptions} OutputOptions */
+
+const fileName = 'index';
+
+/**
+ * @param options {RollupOptions}
+ * @returns {RollupOptions}
+ */
+function bundle(options) {
+  return {
+    ...options,
+    input: `./src/${fileName}.ts`,
+    external: ['thror', 'drino', 'dayjs', '@remscodes/renault-api']
+  }
+}
+
+export default [
+  bundle({
+    plugins: [esbuild(), terser(), bundleSize()],
+    output: [
+      {
+        file: `./dist/cjs/${fileName}.cjs`,
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true
+      },
+      {
+        file: `./dist/esm/${fileName}.mjs`,
+        format: 'es',
+        exports: 'named',
+        sourcemap: true
+      }
+    ]
+  }),
+  bundle({
+    plugins: [dts()],
+    output: {
+      file: `./dist/types/${fileName}.d.ts`,
+      format: 'es'
+    }
+  })
+];
