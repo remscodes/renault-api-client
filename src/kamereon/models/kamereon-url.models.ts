c43278a7a295f2add55de034069ccebb7e92bf66
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2023-11-09 12:34:13 +0100

    Add JSDocs

--- src/kamereon/models/kamereon-url.models.ts
+++ src/kamereon/models/kamereon-url.models.ts
@@ -1,5 +1,8 @@
 import type { KamereonApi } from '@remscodes/renault-api';
 import type { Prefixed } from '../../models/shared.model';
 
+/** @internal **/
 export type ReadApiUrl = keyof Omit<typeof KamereonApi, 'KEY' | Prefixed<'PERFORM_'>>
+
+/** @internal **/
 export type PerformApiUrl = keyof Pick<typeof KamereonApi, Extract<keyof typeof KamereonApi, Prefixed<'PERFORM_'>>>
