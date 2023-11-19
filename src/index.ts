6b97ac5ea455dc574ad3a4ee03fe265c0b406ca0
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2023-11-19 18:01:57 +0100

    Export RenaultSession

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { RenaultClient } from './renault-client';
+export { RenaultSession } from './renault-session';
 export { GigyaClient } from './gigya/gigya-client';
 export { KamereonClient } from './kamereon/kamereon-client';
